36b1b37527bb8eb7432af33d682493ac13e15eb9
Alwifaisal <alwifaisal@Alwis-Faisal.local>
2024-01-02 08:16:48 +0700

    update file

---
+++ testscript.js
@@ -0,0 +1,16 @@
+
+<html>
+<body>
+
+<h1>test Commit</h1>
+<p>Commit Pertama</p>
+<p>halogin</p>
+
+<p id="demo"></p>
+
+<script>
+document.getElementById("demo").innerHTML = 5 + 6;
+</script>
+
+</body>
+</html>
